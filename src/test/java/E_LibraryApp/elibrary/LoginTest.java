933f5aa9db6ef44e32b74395895f8934c841fb9a
Lionel Voillat <lionel.voillat@siemens.com>
2021-06-17 11:50:36 +0200

    Focus

--- src/test/java/E_LibraryApp/elibrary/LoginTest.java
+++ src/test/java/E_LibraryApp/elibrary/LoginTest.java
@@ -153,7 +153,7 @@ public class LoginTest extends TestCase {
 //        options.addArguments("--js-flags=--expose-gc");  
 		
         driver = new RemoteWebDriver(new URL(rmWebDrvURL), options);
-        this.webDriver.manage().window().maximize();
+        driver.manage().window().maximize();
         driver.get(polarionURL);
 		System.out.println("Test - Title is: "+driver.getTitle());
 		
