339e78e721e9d8a3b39b70afd9238281d2bafd16
Lionel Voillat <lionel.voillat@siemens.com>
2022-07-07 09:11:36 +0200

    -

--- src/test/java/E_LibraryApp/elibrary/LoginTest.java
+++ src/test/java/E_LibraryApp/elibrary/LoginTest.java
@@ -171,7 +171,7 @@ public class LoginTest extends TestCase {
 	    
 	    try {
 		    // Anticipate web browser response, with an explicit wait
-// Gecko	    	WebDriverWait wait = new WebDriverWait(driver, 5);
+	    	WebDriverWait wait = new WebDriverWait(driver, 1);
 	    	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("DOM_13")));   // Check for an existing element on the 1st page after login
 	        System.out.println("Message: "+successMsg);
 	    	assertTrue(successMsg,true);
