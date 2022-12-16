9523a40f422e3c576cdb9ae8546107fb4093c1b6
Lionel Voillat <lionel.voillat@siemens.com>
2022-12-16 16:08:26 +0100

    New Polarion 22R2

--- src/test/java/E_LibraryApp/elibrary/LoginTest.java
+++ src/test/java/E_LibraryApp/elibrary/LoginTest.java
@@ -192,8 +192,10 @@ public class LoginTest extends TestCase {
 	    // Polarion Logout
 		TimeUnit.SECONDS.sleep(8);	    
 		WebDriverWait wait4 = new WebDriverWait(driver, 3);
-		wait4.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@class=\"gwt-Image\"]")));		
-		driver.findElement(By.xpath("//img[@class=\"gwt-Image\"]")).click();
+		wait4.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[contains(@src, 'lite_settings_off.svg')]")));		
+		driver.findElement(By.xpath("//img[contains(@src, 'lite_settings_off.svg')]")).click();
+//		wait4.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//img[@class=\"gwt-Image\"]")));		
+//		driver.findElement(By.xpath("//img[@class=\"gwt-Image\"]")).click();
 
 		WebDriverWait wait5 = new WebDriverWait(driver, 3);
 		wait5.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[text()=\"Logout\"]")));	
